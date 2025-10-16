4b6323f278af688586b6fc9f874ae1ba94363d7b
kristinadd <kristina@productboard.com>
2025-10-16 18:34:26 +0200

    Init Gem

---
+++ lib/slack_notifier.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "slack_notifier/version"
+
+module SlackNotifier
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/slack_notifier_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SlackNotifier do
+  it "has a version number" do
+    expect(SlackNotifier::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
